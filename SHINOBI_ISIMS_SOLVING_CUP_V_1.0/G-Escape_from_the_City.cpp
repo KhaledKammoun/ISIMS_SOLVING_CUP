710771a0d4f819293156ba1f6a9815bed1565349
KhaledKammoun <khaledsfax03@gmail.com>
2024-03-06 21:38:50 +0100

    C++

--- SHINOBI_ISIMS_SOLVING_CUP_V_1.0/G-Escape_from_the_City.cpp
+++ SHINOBI_ISIMS_SOLVING_CUP_V_1.0/G-Escape_from_the_City.cpp
@@ -1,45 +1,56 @@
 
 #include <bits/stdc++.h>
-#include <fstream>
 
 #define FAST ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 #define newL cout<<endl
 #define ll long long
 using namespace std ;
+const vector<pair<int,int>> direction {{1,0}, {-1,0},{0,1},{0,-1}};
+bool win = false ;
+void dfs(vector<vector<bool>> v, vector<vector<bool>> visited, int n, int m, int i, int j) {
+    if (i == n-1 && j == m-1 && v[i][j]){
+        win = true ;
+        return ;
+    }
+
+    if (i < 0 || i>=n || j < 0 || j>=m || visited[i][j] || !v[i][j]){
+        return ;
+    }
+    visited[i][j] = true ;
+    for (auto x : direction){
+        int row = x.first, col = x.second ;
+        dfs(v, visited, n,m,i+row, j+col) ;
+        if (win) 
+            return;
+    }
+    return ;
+}
 
 int main(){
     FAST ;
     int n, m ;
     cin>>n>>m ;
-    vector<vector<int>> v(n + 1, vector<int> (m+1, 0)) ;
-    for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= m; j++) {
+    int row = 0, col = 0;
+    vector<vector<bool>> v(n , vector<bool> (m, 0)) ;
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++) {
             char c ;
             cin>>c ;
-            if (c == 'N')
-                v[i][j] = 2 ;
+            if (c == 'N'){
+                row = i ;
+                col = j ;
+                v[i][j] = true ;
+            }
             else if (c == 'D')
-                v[i][j] = 1 ;
+                v[i][j] = true ;
             else
-                v[i][j] = 0 ;    
+                v[i][j] = false ;    
         }
     }
-    for (int i = 1; i <= n ; i++) {
-        for (int j = 1; j <= m; j++) {
-            if ((v[i-1][j] == 2 || v[i][j-1] == 2) && v[i][j] == 1){
-                v[i][j] = 2 ;
-                if (v[i-1][j] == 1){
-                    for (int k = j; k <= m && v[i-1][k] != 0; k++){
-                        v[i-1][k] = 2 ;
-                    }
-                }
-            }
-            else {
-                break ;
-            }
-        }
-    }
-    if (v[n][m] == 2) 
+    
+    vector<vector<bool>> visited(n, vector<bool>(m)) ;
+    dfs(v, visited, n, m, row, col) ;
+    if (win) 
         cout<<"Win"<<endl ;
     else 
         cout<<"Lose"<<endl ;
